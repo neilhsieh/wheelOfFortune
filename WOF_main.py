f80c38aee8e713b88b9ca214669c3c178ccaa467
Neil <neil@Carries-MacBook-Air.local>
2018-10-20 19:15:04 -0400

    welcome title

---
+++ WOF_main.py
@@ -0,0 +1,15 @@
+import time
+
+#Welcome title beg
+welcome = "Welcome to Wheel of Fortune"
+for i in range(len(welcome)+4):
+	print (".", end="")
+
+print ("")
+print( ".." + welcome + "..")
+
+for i in range(len(welcome)+4):
+	print (".", end="")
+print ("")
+#welcome end
+
